306fe20296864287fcce0971efec4745841697be
kambioss <kambia.pouwedeou@oss.org.tn>
2025-10-10 13:47:01 +0100

    read the docs error fix3

--- conf.py
+++ conf.py
@@ -43,3 +43,9 @@ html_theme_options = {
 
 html_static_path = ['_static']
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import os
+
+if os.environ.get('READTHEDOCS') == 'True':
+    html_output_dir = os.path.join(os.environ.get('READTHEDOCS_OUTPUT', '_build'), 'html')
+    if not os.path.exists(html_output_dir):
+        os.makedirs(html_output_dir)
