98410917a2c37749e268432111d0264129b62591
kambioss <kambia.pouwedeou@oss.org.tn>
2025-10-10 14:24:22 +0100

    read the docs error fix3

--- conf.py
+++ conf.py
@@ -45,7 +45,8 @@ html_static_path = ['_static']
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 import os
 
-if os.environ.get('READTHEDOCS') == 'True':
-    html_output_dir = os.path.join(os.environ.get('READTHEDOCS_OUTPUT', '_build'), 'html')
-    if not os.path.exists(html_output_dir):
-        os.makedirs(html_output_dir)
+# Set the output directory for HTML builds
+if 'READTHEDOCS' in os.environ:
+    html_dir = os.path.join(os.environ.get('READTHEDOCS_OUTPUT', ''), 'html')
+else:
+    html_dir = '_build/html'
